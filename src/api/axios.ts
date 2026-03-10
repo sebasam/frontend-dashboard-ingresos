fb6950b0a74a7141767aefb7f51c22d60b8a0aa9
juanamaya <juan.amaya@ottomotoapp.com>
2026-03-10 14:00:35 -0500

    changing url to prod

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://backend-financial-dashboard-cfb572b3f6e5.herokuapp.com/api",
     headers: { "Content-Type": "application/json" },
 });
 
